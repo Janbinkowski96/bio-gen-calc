448729b20ea524626a53ecc8906a9e3b1e977c3f
Szymon Miks <szymonm@miroart.pl>
2018-10-16 15:42:34 +0200

    i'm trying to add webpack to project and divide css to separete smaller chunks

--- app/static/webpack.config.js
+++ app/static/webpack.config.js
@@ -1,13 +1,32 @@
-const webpack = require('webpack');
-
-const config = {
-    entry:  __dirname + '/js/app.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.js', '.css']
-    },
-};
-module.exports = config;
+// webpack v4
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+  entry: { main: './js/app.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader']
+          })
+      }
+    ]
+  },
+  plugins: [ 
+    new ExtractTextPlugin({filename: 'style.css'})
+  ]
+};;
